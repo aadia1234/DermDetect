b3f7fcb9f785c45dfa596bb309d324b24ca2b3e1
gam3boy1 <aadianand1234@gmail.com>
2023-04-18 20:46:10 +0800

    updated contentview

--- DermDetect.swiftpm/Views/ContentView.swift
+++ DermDetect.swiftpm/Views/ContentView.swift
@@ -3,7 +3,7 @@ import AVKit
 
 
 struct ContentView: View {
-    
+    // Please build this app with Xcode 14 on macOS 13, and run it on an iPad with iPadOS 16
     var body: some View {
         NavigationStack {
             WelcomeView()
